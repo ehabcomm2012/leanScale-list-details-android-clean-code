aa9c2f8ee29546945590318b9d47faa7fc8e4894
Ehab Shabaan <ehab.shaaban@emiratesauction.ae>
2021-08-07 19:28:42 +0200

    finalize gui of ProductDetailsFragment xml

--- app/src/main/java/com/dubizzle/listdetails/features/productDetails/ProductDetailsFragment.kt
+++ app/src/main/java/com/dubizzle/listdetails/features/productDetails/ProductDetailsFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
+import com.dubizzle.listdetails.R
 import com.dubizzle.listdetails.core.baseUi.BaseFragment
 import com.dubizzle.listdetails.core.baseUi.BaseViewModel
 import com.dubizzle.listdetails.databinding.FragmentProductDetailsBinding
@@ -23,7 +26,8 @@ class ProductDetailsFragment : BaseFragment() {
     private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        productItem = arguments?.getSerializable(ProductListFragment.PRODUCT_DETAILS_KEY) as Product?
+        productItem =
+            arguments?.getSerializable(ProductListFragment.PRODUCT_DETAILS_KEY) as Product?
     }
 
     override fun onCreateView(
@@ -41,8 +45,20 @@ class ProductDetailsFragment : BaseFragment() {
     }
 
     private fun bindData() {
-        _binding?.tvRate?.text = productItem?.name
-        _binding?.tvCountry?.text = productItem?.price
+        _binding?.toolbar?.title = productItem?.name
+        _binding?.collapsingToolbarLayout?.contentScrim =
+            ContextCompat.getDrawable(requireContext(), R.color.teal_200)
+
+        Glide
+            .with(requireContext())
+            .load(productItem?.image_urls_thumbnails?.get(0))
+            .centerCrop()
+            .into(_binding?.ivProduct!!)
+        _binding?.tvTitle?.text = productItem?.name
+        _binding?.tvShortDesc?.text = productItem?.created_at
+        _binding?.tvPrice?.text = productItem?.price
+        _binding?.tvLotPrice?.text = productItem?.price
+
     }
 
     override fun subscribeObservers() {
